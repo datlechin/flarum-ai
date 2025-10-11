31834b3eff7edb10452dda3a4fd872272f142833
Ngo Quoc Dat <datlechin@gmail.com>
2025-10-11 10:09:53 +0700

    lint

--- js/src/admin/index.ts
+++ js/src/admin/index.ts
@@ -2,7 +2,5 @@ import app from 'flarum/admin/app';
 import AISettingsPage from './components/AISettingsPage';
 
 app.initializers.add('nqd/ai-core', () => {
-  app.extensionData
-    .for('datlechin-ai')
-    .registerPage(AISettingsPage);
+  app.extensionData.for('datlechin-ai').registerPage(AISettingsPage);
 });
